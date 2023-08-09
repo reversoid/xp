f9c0733b77821213cc32b0a2ae4586c50734f555
George Kobunov <ancient.gekob@gmail.com>
2023-08-09 14:09:48 +0300

    Some setup

--- xp-backend/src/app.module.ts
+++ xp-backend/src/app.module.ts
@@ -5,9 +5,32 @@ import { ExperimentModule } from './modules/experiment/experiment.module';
 import { ObservationModule } from './modules/observation/observation.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProtectedModule } from './modules/protected/protected.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getPostgresConfig } from './config/modules-configs/getPostgresConfig';
+import { getRedisConfig } from './config/modules-configs/getRedisConfig';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { ConfigModule } from '@nestjs/config';
+import postgresConfig from './config/postgres.config';
+import redisConfig from './config/redis.config';
+import secretsConfig from './config/secrets.config';
+import globalConfig from './config/global.config';
 
 @Module({
-  imports: [ExperimentModule, ObservationModule, AuthModule, ProtectedModule],
+  imports: [
+    TypeOrmModule.forRootAsync(getPostgresConfig()),
+    RedisModule.forRootAsync(getRedisConfig()),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [postgresConfig, redisConfig, secretsConfig, globalConfig],
+      ignoreEnvFile: false,
+      envFilePath: ['config/.env'],
+      cache: true,
+    }),
+    ExperimentModule,
+    ObservationModule,
+    AuthModule,
+    ProtectedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
