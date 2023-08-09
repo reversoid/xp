8b92f65a9541a613269de00fbe75c31bcfd7c474
George Kobunov <ancient.gekob@gmail.com>
2023-08-09 16:04:09 +0300

    Add statuses for experiments

--- xp-backend/src/modules/experiment/entities/experiment.entity.ts
+++ xp-backend/src/modules/experiment/entities/experiment.entity.ts
@@ -11,6 +11,12 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+export enum ExperimentStatus {
+  STARTED = 'STARTED',
+  COMPLETED = 'COMPLETED',
+  CANCELED = 'CANCELED',
+}
+
 @Entity()
 export class Experiment {
   @PrimaryGeneratedColumn()
@@ -26,25 +32,38 @@ export class Experiment {
   text: string;
 
   @Column('varchar', { nullable: true, length: 256 })
-  tg_photo_id: string;
+  tg_photo_id?: string;
 
   @Column('varchar', { nullable: true, length: 256 })
-  tg_video_id: string;
+  tg_video_id?: string;
 
   @Column('varchar', { nullable: true, length: 256 })
-  tg_video_note_id: string;
+  tg_video_note_id?: string;
 
   @Column('varchar', { nullable: true, length: 256 })
-  tg_voice_id: string;
+  tg_voice_id?: string;
 
   @Column('varchar', { nullable: true, length: 256 })
-  tg_document_id: string;
+  tg_document_id?: string;
 
   @Column('jsonb', { nullable: true, array: true })
-  tg_media_group: MediaGroupItem[];
+  tg_media_group?: MediaGroupItem[];
 
   @Column('jsonb', { nullable: true })
-  geo: Geo;
+  geo?: Geo;
+
+  @Column('enum', {
+    enum: ExperimentStatus,
+    default: ExperimentStatus.STARTED,
+    nullable: false,
+  })
+  status: ExperimentStatus;
+
+  @Column('timestamptz', { nullable: true })
+  completed_at?: number;
+
+  @Column('timestamptz', { nullable: false })
+  complete_by: Date;
 
   @Index()
   @CreateDateColumn({ select: false, type: 'timestamptz' })
