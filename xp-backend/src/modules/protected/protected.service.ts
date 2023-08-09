8f5603d41801d0ff9cc22ccb7f4992314ed61354
George Kobunov <ancient.gekob@gmail.com>
2023-08-09 19:53:53 +0300

    Implement getting unfinished experiments

--- xp-backend/src/modules/protected/protected.service.ts
+++ xp-backend/src/modules/protected/protected.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { ExperimentRepository } from '../experiment/repository/experiment.repository';
-import { MoreThan } from 'typeorm';
 import { DateTime } from 'luxon';
 import {
   Experiment,
   ExperimentStatus,
 } from '../experiment/entities/experiment.entity';
+import { ExperimentRepository } from '../experiment/repository/experiment.repository';
 
 @Injectable()
 export class ProtectedService {
   constructor(private experimentRepository: ExperimentRepository) {}
 
   async getAllUnfinishedExperiments(): Promise<Experiment[]> {
-    const experiments = await this.experimentRepository.find({
-      where: {
-        completed_at: MoreThan(DateTime.now().toJSDate()),
+    const experiments = await this.experimentRepository
+      .createQueryBuilder('experiment')
+      .select([
+        'experiment.complete_by',
+        'experiment.id',
+        'experiment.status',
+        'experiment.created_at',
+      ])
+      .addSelect('user.id', 'userId')
+      .leftJoin('experiment.user', 'user')
+      .where('experiment.complete_by > :completeBy', {
+        completeBy: DateTime.now().toJSDate(),
+      })
+      .andWhere('experiment.status = :status', {
         status: ExperimentStatus.STARTED,
-      },
-    });
+      })
+      .getMany();
 
     return experiments;
   }
