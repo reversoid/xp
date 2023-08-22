4b76780ddaf1e9c76e7fe27182e586979297d8e1
George Kobunov <ancient.gekob@gmail.com>
2023-08-22 16:29:29 +0300

    Removed redis from backend, add example.env

--- xp-backend/src/app.module.ts
+++ xp-backend/src/app.module.ts
@@ -5,12 +5,9 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ProtectedModule } from './modules/protected/protected.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getPostgresConfig } from './config/modules-configs/getPostgresConfig';
-// import { getRedisConfig } from './config/modules-configs/getRedisConfig';
-// import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import postgresConfig from './config/postgres.config';
-import redisConfig from './config/redis.config';
 import secretsConfig from './config/secrets.config';
 import globalConfig from './config/global.config';
 import { TaskModule } from './modules/task/task.module';
@@ -20,10 +17,9 @@ import { FeedModule } from './modules/feed/feed.module';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(getPostgresConfig()),
-    // RedisModule.forRootAsync(getRedisConfig()), // TODO use if necessary
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [postgresConfig, redisConfig, secretsConfig, globalConfig],
+      load: [postgresConfig, secretsConfig, globalConfig],
       ignoreEnvFile: false,
       envFilePath: ['config/.env'],
       cache: true,
