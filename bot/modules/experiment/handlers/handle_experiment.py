f6b6510e98f3a7539003ee370d9c51d38a911a71
George Kobunov <ancient.gekob@gmail.com>
2023-08-24 00:35:35 +0300

    Handle media groups

--- bot/modules/experiment/handlers/handle_experiment.py
+++ bot/modules/experiment/handlers/handle_experiment.py
@@ -1,22 +1,35 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command, StateFilter
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from modules.core.middlewares.AlbumMiddleware import AlbumMiddleware
 
 from modules.experiment.lexicon import LEXICON
 from modules.experiment.states import FSMExperiment
-from shared.utils.convert.message_to_upload_request import process_message_files
+from shared.utils.convert.message_to_upload_request import process_message_files, process_media_group_files
 from shared.my_types import UploadInfoRequest
 
 router: Router = Router()
 
+router.message.middleware.register(AlbumMiddleware())
 
-@router.message(StateFilter(FSMExperiment.completing))
+
+@router.message(StateFilter(FSMExperiment.completing), F.media_group_id == None)
 async def handle_experiment(message: Message, state: FSMContext):
     data = await state.get_data()
-    # TODO apply middleware that reveals media group 
     messages: list[UploadInfoRequest] = data.get('messages', [])
     newRequest = process_message_files(message)
+    messages.append(newRequest.model_dump())
+
+    await state.update_data(messages=messages)
+    await message.answer(text=LEXICON['continue_experiment'])
+    # TODO check for maximim messages length (should not be greater than 10)
+
+@router.message(StateFilter(FSMExperiment.completing), F.media_group_id != None)
+async def handle_media_group(message: Message, state: FSMContext, album: list[Message]):
+    data = await state.get_data()
+    messages: list[UploadInfoRequest] = data.get('messages', [])
+    newRequest = process_media_group_files(album)
     messages.append(newRequest.model_dump())
 
     await state.update_data(messages=messages)
