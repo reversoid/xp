249c12b905df6939e8b10036904dcd1e3e579f15
George Kobunov <ancient.gekob@gmail.com>
2023-08-31 14:29:51 +0300

    fix texting

--- bot/modules/profile/lexicon/__init__.py
+++ bot/modules/profile/lexicon/__init__.py
@@ -14,7 +14,7 @@ LEXICON: Lexicon = {
     'about_profile_command': 'Welcome to profile',
     'send_username': 'Send an xp-username to follow',
     'no_username_provided': 'Please, send a username',
-    'no_such_user': 'There is no such user',
+    'no_such_user': 'There is no such user. Try again',
     'followed_successfully': 'You were successfully subscribed!',
     'already_subscribed': 'You are already subscribed to the user',
 }
