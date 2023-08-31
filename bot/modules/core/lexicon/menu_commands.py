16bc219dca8276e488feca7442f96fe04d5bb038
George Kobunov <ancient.gekob@gmail.com>
2023-08-31 15:08:58 +0300

    Add profile to menu

--- bot/modules/core/lexicon/menu_commands.py
+++ bot/modules/core/lexicon/menu_commands.py
@@ -1,5 +1,6 @@
 MENU_COMMANDS = {
+    '/profile': 'Your profile',
     '/log_observation': 'Capture what makes you curious',
     '/run_experiment': 'Transform observations into something unexpected',
-    '/help': 'Get the info about this bot'
+    '/help': 'Get the info about this bot',
 }
