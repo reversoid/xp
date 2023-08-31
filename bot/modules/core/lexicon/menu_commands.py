edc526c6d7c01657b89995bb29fad49f23c9ad9a
George Kobunov <ancient.gekob@gmail.com>
2023-09-01 00:14:09 +0300

    Add some menu commands

--- bot/modules/core/lexicon/menu_commands.py
+++ bot/modules/core/lexicon/menu_commands.py
@@ -4,4 +4,7 @@ MENU_COMMANDS = {
     '/log_observation': 'Capture what makes you curious',
     '/run_experiment': 'Transform observations into something unexpected',
     '/help': 'Get the info about this bot',
+    '/my_experiments': 'Your experiments',
+    '/my_observations': 'Your observations',
+    '/follow': 'Follow somebody',
 }
