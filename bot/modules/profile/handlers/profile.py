423d72ccba576503c4f87fe50a37d8983a9cf86c
George Kobunov <ancient.gekob@gmail.com>
2023-09-01 17:44:57 +0300

    Show info on profile

--- bot/modules/profile/handlers/profile.py
+++ bot/modules/profile/handlers/profile.py
@@ -1,13 +1,17 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from modules.auth.services import CheckUserRegistrationResponse, auth_service
 
 from modules.profile.lexicon import LEXICON
 from modules.profile.keyboards.profile_keyboard import profile_keyboard
+from modules.profile.services import profile_service
 
 profile_router = Router()
 
 
 @profile_router.message(Command('profile'))
 async def handle_feed_command(message: Message):
-    await message.answer(text=LEXICON['about_profile_command'], reply_markup=profile_keyboard)
+    response: CheckUserRegistrationResponse = await auth_service.is_user_registered(tg_user_id=message.from_user.id)
+
+    await message.answer(text=f'Hello, {response.username}', reply_markup=profile_keyboard)
