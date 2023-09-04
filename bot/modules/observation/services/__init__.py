30e6fc5ed612beaa2b6bd51776b788323816606f
George Kobunov <ancient.gekob@gmail.com>
2023-09-04 19:26:26 +0300

    Fix file upload for observation log

--- bot/modules/observation/services/__init__.py
+++ bot/modules/observation/services/__init__.py
@@ -19,7 +19,7 @@ class ObservationService(ApiService):
         return request.model_dump()
 
     def __validate_created_observation(self, request: UploadInfoRequest):
-        if not request.text and not request.photo_id and not request.video_id and not request.voice_id and not request.video_note_id:
+        if not request.text and not request.photo_id and not request.video_id and not request.voice_id and not request.video_note_id and not request.document_id:
             raise NoDataForObservation
 
 observation_service = ObservationService()
