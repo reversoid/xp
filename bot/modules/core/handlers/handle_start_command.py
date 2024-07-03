0c5574c66f5b405feb881eb213c5628345b7eb08
reversoid <ancient.gekob@gmail.com>
2024-07-03 14:54:26 +0300

    Fix some format

--- bot/modules/core/handlers/handle_start_command.py
+++ bot/modules/core/handlers/handle_start_command.py
@@ -17,36 +17,45 @@ async def handle_start_command(message: Message, state: FSMContext):
     await state.clear()
     response = await auth_service.is_user_registered(message.from_user.id)
 
-    if (response.registered):
-        await message.answer(text=f'Привет, {response.username}! \n\n{CORE_LEXICON["cmd_start"]}', reply_markup=ReplyKeyboardRemove())
+    if response.registered:
+        await message.answer(
+            text=f'Привет, {response.username}! \n\n{CORE_LEXICON["cmd_start"]}',
+            reply_markup=ReplyKeyboardRemove(),
+        )
         # TODO check if user has running experiment
     else:
         await state.set_state(FSMRegistration.fill_username)
-        await message.answer(text=CORE_LEXICON['cmd_start'], reply_markup=ReplyKeyboardRemove())
-        await message.answer(text=CORE_LEXICON['fill_username'])
+        await message.answer(
+            text=CORE_LEXICON["cmd_start"], reply_markup=ReplyKeyboardRemove()
+        )
+        await message.answer(text=CORE_LEXICON["fill_username"])
 
 
-@start_router.message(StateFilter(FSMRegistration.fill_username), F.text.regexp(username_pattern))
+@start_router.message(
+    StateFilter(FSMRegistration.fill_username), F.text.regexp(username_pattern)
+)
 async def handle_input_username(message: Message, state: FSMContext):
     try:
         await auth_service.register(message.from_user.id, message.text)
-        await message.answer(text=CORE_LEXICON['welcome'])
+        await message.answer(text=CORE_LEXICON["welcome"])
         await state.clear()
 
     except UserExistsException:
-        await message.answer(text=CORE_LEXICON['username_already_taken'])
+        await message.answer(text=CORE_LEXICON["username_already_taken"])
 
     except Exception:
-        await message.answer(text=SHARED_LEXICON['internal_error'])
+        await message.answer(text=SHARED_LEXICON["internal_error"])
 
 
 # If wrong text of username is given
-@start_router.message(StateFilter(FSMRegistration.fill_username), ~F.text.regexp(username_pattern))
+@start_router.message(
+    StateFilter(FSMRegistration.fill_username), ~F.text.regexp(username_pattern)
+)
 async def handle_input_wrong_username(message: Message):
-    await message.answer(text=CORE_LEXICON['wrong_username_format'])
+    await message.answer(text=CORE_LEXICON["wrong_username_format"])
 
 
 # If wrong message is given
 @start_router.message(StateFilter(FSMRegistration.fill_username))
 async def handle_input_wrong_message(message: Message):
-    await message.answer(text=CORE_LEXICON['bad_username_message'])
+    await message.answer(text=CORE_LEXICON["bad_username_message"])
