d2d8935970b2e87d7cf47d11909623e531708755
reversoid <ancient.gekob@gmail.com>
2024-07-03 23:54:01 +0300

    Cached shpere

--- bot/modules/core/handlers/handle_start_command.py
+++ bot/modules/core/handlers/handle_start_command.py
@@ -1,14 +1,27 @@
 from aiogram import Bot, Dispatcher, Router, F
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.filters import CommandStart
+from config.config import load_config
 from modules.core.lexicon import CORE_LEXICON
 from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile
 
 from modules.auth.services import auth_service
 from modules.profile.services import profile_service
+from aiogram.fsm.storage.redis import Redis
 
 start_router: Router = Router()
+
+
+FILE_ID_KEY = "welcome_file_id"
+
+config = load_config()
+
+redis = Redis(
+    host=config.database.redis.host,
+    password=config.database.redis.password,
+    port=config.database.redis.port,
+)
 
 
 @start_router.message(CommandStart())
@@ -27,11 +40,18 @@ async def handle_start_command(
     if not user:
         await auth_service.register(tg_user_id, tg_username)
 
-    # TODO cache file id
-    file = FSInputFile("static/sphere.mp4")
+    existing_file_id = await redis.get(FILE_ID_KEY)
+    file = (
+        existing_file_id.decode()
+        if existing_file_id
+        else FSInputFile("static/sphere.mp4")
+    )
 
     result = await message.answer_video(
         video=file,
         caption=f'Привет, {tg_username} \n\n{CORE_LEXICON["cmd_start"]}',
         reply_markup=ReplyKeyboardRemove(),
     )
+
+    if not existing_file_id:
+        await redis.set(FILE_ID_KEY, result.video.file_id)
