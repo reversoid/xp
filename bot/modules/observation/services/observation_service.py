9e6304d14914e9e7704574a032d9cd98f2a3ae02
reversoid <ancient.gekob@gmail.com>
2024-07-03 14:49:02 +0300

    Allow observation media groups

--- bot/modules/observation/services/observation_service.py
+++ bot/modules/observation/services/observation_service.py
@@ -4,6 +4,7 @@ from core.api_services.observation_api_service import (
     observation_api_service,
 )
 from core.models import TgGeo
+from core.models.tg_media_group_item import TgMediaGroupItem
 from .exceptions import NoDataForObservation
 
 
@@ -22,35 +23,56 @@ class ObservationService:
             tg_user_id, observation_id
         )
 
-    def __message_to_observation_dto(self, message: Message) -> CreateObservationDto:
-        text = message.text or message.caption
-        video_id = message.video.file_id if message.video else None
-        photo_id = message.photo[-1].file_id if message.photo else None
-        document_id = message.document.file_id if message.document else None
-        geo = (
-            TgGeo(
-                longitude=message.location.longitude,
-                latitude=message.location.latitude,
-                horizontalAccuracy=message.location.horizontal_accuracy,
+    def __message_to_observation_dto(
+        self, messages: list[Message]
+    ) -> CreateObservationDto:
+        dto: CreateObservationDto
+
+        if len(messages) == 1:
+            message = messages[0]
+            text = message.text or message.caption
+            video_id = message.video.file_id if message.video else None
+            photo_id = message.photo[-1].file_id if message.photo else None
+            document_id = message.document.file_id if message.document else None
+            geo = (
+                TgGeo(
+                    longitude=message.location.longitude,
+                    latitude=message.location.latitude,
+                    horizontalAccuracy=message.location.horizontal_accuracy,
+                )
+                if message.location
+                else None
             )
-            if message.location
-            else None
-        )
-        voice_id = message.voice.file_id if message.voice else None
-        video_note_id = message.video_note.file_id if message.video_note else None
+            voice_id = message.voice.file_id if message.voice else None
+            video_note_id = message.video_note.file_id if message.video_note else None
+            dto = CreateObservationDto(
+                tgText=text,
+                tgPhotoId=photo_id,
+                tgVideoId=video_id,
+                tgDocumentId=document_id,
+                tgGeo=geo,
+                tgVoiceId=voice_id,
+                tgVideoNoteId=video_note_id,
+                tgMediaGroup=None,
+            )
 
-        observation = CreateObservationDto(
-            tgText=text,
-            tgDocumentId=document_id,
-            tgGeo=geo,
-            tgPhotoId=photo_id,
-            tgVideoId=video_id,
-            tgVoiceId=voice_id,
-            tgVideoNoteId=video_note_id,
-            tgMediaGroup=None,
-        )
+        else:  # For media group
+            text = ""
+            media_group: list[TgMediaGroupItem] = []
 
-        return observation
+            for message in messages:
+                text += message.text or message.caption
+                media_group_item = TgMediaGroupItem(
+                    tgAudioId=message.audio.file_id,
+                    tgDocumentId=message.document.file_id,
+                    tgPhotoId=message.photo[-1].file_id,
+                    tgVideoId=message.video.file_id,
+                )
+                media_group.append(media_group_item)
+
+            dto = CreateObservationDto(tgText=text, tgMediaGroup=media_group_item)
+
+        return dto
 
     def __validate_new_observation_dto(self, dto: CreateObservationDto):
         if (
@@ -61,6 +83,7 @@ class ObservationService:
             and not dto.tgVideoNoteId
             and not dto.tgDocumentId
             and not dto.tgGeo
+            and not dto.tgMediaGroup
         ):
             raise NoDataForObservation
 
