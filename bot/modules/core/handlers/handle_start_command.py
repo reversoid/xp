ac1651628cc59b2e7e782c14481f7a8e72b8c663
reversoid <ancient.gekob@gmail.com>
2024-07-03 23:45:09 +0300

    Implement sphere on start

--- bot/modules/core/handlers/handle_start_command.py
+++ bot/modules/core/handlers/handle_start_command.py
@@ -1,22 +1,22 @@
-from aiogram import Router, F
+from aiogram import Bot, Dispatcher, Router, F
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.filters import CommandStart
 from modules.core.lexicon import CORE_LEXICON
 from aiogram.fsm.context import FSMContext
-
+from aiogram.types import FSInputFile
 
 from modules.auth.services import auth_service
 from modules.profile.services import profile_service
 
 start_router: Router = Router()
 
-WELCOME_GIF_ID = (
-    "CgACAgIAAxkBAAIIkWaC-mZtOFXVSZERgxKJasvkZqE4AAKoUwACyAURSFIoxT4AASK2gTUE"
-)
-
 
 @start_router.message(CommandStart())
-async def handle_start_command(message: Message, state: FSMContext):
+async def handle_start_command(
+    message: Message,
+    state: FSMContext,
+):
+
     await state.clear()
 
     tg_user_id = message.from_user.id
@@ -27,8 +27,11 @@ async def handle_start_command(message: Message, state: FSMContext):
     if not user:
         await auth_service.register(tg_user_id, tg_username)
 
-    await message.answer_video(
-        video=WELCOME_GIF_ID,
+    # TODO cache file id
+    file = FSInputFile("static/sphere.mp4")
+
+    result = await message.answer_video(
+        video=file,
         caption=f'Привет, {tg_username} \n\n{CORE_LEXICON["cmd_start"]}',
         reply_markup=ReplyKeyboardRemove(),
     )
