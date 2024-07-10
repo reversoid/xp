2d8fb7009ebdddeaa03731d48826916854df8001
reversoid <ancient.gekob@gmail.com>
2024-07-10 21:26:29 +0300

    Remove auth guard

--- xp-api/src/routes/profile/get-profile.ts
+++ xp-api/src/routes/profile/get-profile.ts
@@ -1,10 +1,9 @@
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import { authGuard } from "../../utils/guards/auth.guard.js";
 
 const getProfile: FastifyPluginAsyncZod = async (fastify): Promise<void> => {
   fastify.get(
     "/",
-    { preHandler: [authGuard] },
+
     async function (request, reply) {
       const user = request.user!;
       return reply.send({ user });
