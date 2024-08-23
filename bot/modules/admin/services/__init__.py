41b544827c31d04e6b1e8b124af53ab96f0aa62e
reversoid <ancient.gekob@gmail.com>
2024-08-24 00:51:25 +0300

    Fix upsert subscription method

--- bot/modules/admin/services/__init__.py
+++ bot/modules/admin/services/__init__.py
@@ -1,11 +1,10 @@
 from datetime import datetime, timedelta
 
 from core.api_services.subscription_api_service import subscription_api_service
-from core.models.subscription import Subscription
 
 
 class AdminService:
-    async def upsert_subscription(tg_user_id: int, username: str, days: int):
+    async def upsert_subscription(self, tg_user_id: int, username: str, days: int):
         until = datetime.now() + timedelta(days=days)
         subscription = await subscription_api_service.upsert_subscription(
             tg_user_id=tg_user_id, username=username, until=until
